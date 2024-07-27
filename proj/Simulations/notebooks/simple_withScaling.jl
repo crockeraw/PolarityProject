49ee47d6bc8278b388658262c1a9d6ff1ebe55e9
crockeraw <alex.w.crocker@gmail.com>
2024-07-27 11:57:54 -0400

    code to simulate polarity site number with increasing size

---
+++ proj/Simulations/notebooks/simple_withScaling.jl
@@ -0,0 +1,87 @@
+using DifferentialEquations
+using ModelingToolkit
+using LinearAlgebra
+using Plots
+using Colors
+using Images
+using Statistics
+using Sundials
+using Random
+using JLD2
+
+function setup(r)
+    # Generate constants
+    N = 100
+    SA = 4*pi*r 
+    V = (4/3)*pi*r^2
+    mem_thickness = 0.01
+    n = (mem_thickness * SA) / V
+
+    Ax = Array(Tridiagonal([1.0 for i in 1:N-1],[-2.0 for i in 1:N],[1.0 for i in 1:N-1]))
+    Ax[1,end] = 1.0
+    Ax[end,1] = 1.0
+    dx = (r*sqrt(pi))/N
+    Ax = Ax/(dx^2) # adjust for 1/microns
+    Ay = copy(Ax)
+
+    r0 = zeros(100,100,2)
+    r0[:,:,1] .= 10 .*(rand.())   # Cdc42-GTPm
+    r0[:,:,2] .= .2 - mean(r0[:,:,1])*n   # Cdc42-GDPm
+    
+    # Dummy parameters used only locally in fxn but passed to specify scope, or something..
+    Ayt = zeros(N,N)
+    tAx = zeros(N,N)
+    D42t = zeros(N,N)
+    D42d = zeros(N,N)
+    R = zeros(N,N)
+    dummy = (Ayt, tAx, D42t, D42d, R)
+    # Actual parameters
+    a = 1
+    b = 0.25
+    Dm = 0.01
+    Dc = 10
+    n = n
+
+    p = (a, b, Dm, Dc, n, Ax, Ay, dummy)
+    return p, r0
+end
+
+function simple!(dr,r,p,t)
+    a, b, Dm, Dc, n, Ax, Ay, dummy = p
+    Ayt, tAx, D42t, D42d, R = dummy
+    # Window variables
+    rhoT = @view r[:,:,1]
+    rhoD = @view r[:,:,2]
+    # Calculate diffusion
+    mul!(Ayt,Ay,rhoT)
+    mul!(tAx,rhoT,Ax)
+    @. D42t = Dm*(Ayt + tAx)
+    mul!(Ayt,Ay,rhoD)
+    mul!(tAx,rhoD,Ax)
+    @. D42d = Dc*(Ayt + tAx)
+    # Calculate reactions, add diffusion
+    @. R = (a*rhoT^2*rhoD) - b*rhoT
+    @. dr[:,:,1] = R + D42t
+    @. dr[:,:,2] = -R*n + D42d
+end
+
+function run(radius)
+    p, r0 = setup(radius)
+    min_prob = ODEProblem(simple!,r0,(0.0,1800),p)
+    sol_simp = solve(min_prob,CVODE_BDF(linear_solver = :GMRES), saveat=(599,600))
+    @save "sims/simple_30min_seed_$(seed)_radius_$(radius).jld2" sol_simp
+end
+
+seed = 5
+Random.seed!(seed)
+
+print("running...")
+run(10)
+run(8)
+run(7)
+run(6)
+run(5)
+run(4)
+run(3)
+run(2)
+print("complete!")
